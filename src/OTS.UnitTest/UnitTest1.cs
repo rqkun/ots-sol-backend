4dabc08a1de41a282ea2d29e80b31c428b89fc3e
raqkun <nhkduy0809@gmail.com>
2024-08-12 20:20:46 +0700

    added missing

---
+++ src/OTS.UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace OTS.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
