a5e952b9f567a62519669cff3d68469d71814cb6
raqkun <nhkduy0809@gmail.com>
2024-08-21 13:36:02 +0700

    add entities, claims

---
+++ src/OTS.Data/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTS.Data.Entities
+{
+    public class User : IdentityUser<Guid>
+    {
+        [Column("AvatarSeed")]
+        public string? AvatarSeed {  get; set; }
+
+        [Column("IsDeleted")]
+        public bool IsDeleted {  get; set; }
+
+    }
+}
