7422558d26ad6d7705059642ee32ac52bf4f5e15
raqkun <nhkduy0809@gmail.com>
2024-08-23 11:22:46 +0700

    added blacklist, user, role viewmodels

---
+++ src/OTS.Data/ViewModels/BlacklistRelatedViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTS.Data.ViewModels
+{
+    public class BlackListViewModel
+    {
+        public Guid BlacklistId { get; set; }
+        public Guid? UserId { get; set; }
+        public string? Username { get; set; }
+        public DateTime? EntryDate { get; set; }
+        public string? Reason { get; set; }
+    }
+    public class BlackListEntriesViewModel
+    {
+        public int? TotalCount { get; set; }
+        public virtual ICollection<BlackListViewModel>? EntryList { get; set; }
+    }
+}
