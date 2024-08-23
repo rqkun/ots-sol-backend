f8c3732719efa7f7ef820c7fe078c2d7b17b4cf8
raqkun <nhkduy0809@gmail.com>
2024-08-23 11:09:27 +0700

    added role, user models

---
+++ src/OTS.Data/Models/UserRelatedModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTS.Data.Models
+{
+    public class Usermodel
+    {
+        public string? Email { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public bool IsDeleted { get; set; }
+        public string? AvatarSeed { get; set; }
+    }
+    public class CreateUserModel
+    {
+        [Required]
+        public string Username { get; set; } = null!;
+        [Required, EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string Password { get; set; } = null!;
+        [Required]
+        public string ConfirmPassword { get; set; } = null!;
+        public string? AvatarSeed { get; set; }
+
+    }
+    public class SignUpModel
+    {
+        [Required]
+        public string Username { get; set; } = null!;
+        [Required, EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string Password { get; set; } = null!;
+        [Required]
+        public string ConfirmPassword { get; set; } = null!;
+        public string? AvatarSeed { get; set; }
+
+    }
+    public class SignInModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+
+    public class ResetPasswordModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string NewPassword { get; set; }
+        [Required]
+        public string ConfirmPassword { get; set; }
+    }
+}
